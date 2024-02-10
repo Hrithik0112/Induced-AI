a3d0110c86f4e62646f61d69eef8adc3d556c74c
Hrithik <slumdog4321@gmail.com>
2024-02-11 00:14:48 +0530

    some footer css

--- components/section/Footer.tsx
+++ components/section/Footer.tsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
   return (
-    <footer className="pt-24 pb-14 relative bg-footer-gradient">
+    <footer className="pt-24 pb-14 relative bg-footer-gradient bg-white/40">
       <section className="mx-14 px-4">
         <main className="pb-12">Footer Content goes here</main>
         <p className="text-lg text-white/40">© All rights reserved, Induced AI</p>
