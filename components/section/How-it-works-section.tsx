4a59a099eeffaf1b25ba9da1f9bce23f0d571c7e
Hrithik <slumdog4321@gmail.com>
2024-02-15 23:32:45 +0530

    grid layout fix

--- components/section/How-it-works-section.tsx
+++ components/section/How-it-works-section.tsx
@@ -8,8 +8,11 @@ export const HowItWorks = () => {
             AUTOMATE & DELEGATE TO INDUCED
           </div>
         </div>
-        <div className="w-full h-full mt-5 grid grid-cols-2 justify-between items-center gap-10 ">
-          <div id="table-content" className="bg-medium-blue text-white ">
+        <div className="w-full h-full mt-5 grid grid-cols-2 place-items-center">
+          <div
+            id="table-content"
+            className="w-[45%] h-[600px] ml-3 bg-medium-blue text-white block overflow-hidden relative "
+          >
             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
             has been the industry's standard dummy text ever since the 1500s, when an unknown
             printer took a galley of type and scrambled it to make a type specimen book. It has
@@ -18,7 +21,10 @@ export const HowItWorks = () => {
             Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
             publishing software like Aldus PageMaker including versions of Lorem Ipsum.
           </div>
-          <div id="table-data" className="bg-white text-black ">
+          <div
+            id="table-data"
+            className="bg-white text-black w-[45%] h-[600px] ml-3 flex flex-col items-start  "
+          >
             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
             has been the industry's standard dummy text ever since the 1500s, when an unknown
             printer took a galley of type and scrambled it to make a type specimen book. It has
